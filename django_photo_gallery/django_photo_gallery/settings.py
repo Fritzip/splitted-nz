3aa2d940aa38d6d037029fbc83e6693182432d6a
Maxime Sainlot <maxime.sainlot@gmail.com>
2019-10-24 20:23:14 +1300

    local access

--- django_photo_gallery/django_photo_gallery/settings.py
+++ django_photo_gallery/django_photo_gallery/settings.py
@@ -10,11 +10,13 @@ SECRET_KEY = '8ace3072-47a0-4910-b522-dc3601f38c35'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0','127.0.0.1','localhost','farges.ddns.net','django-photo-gallery.velingeorgiev.pro']
+ALLOWED_HOSTS = ['0.0.0.0','127.0.0.1','localhost','farges.ddns.net','192.168.1.18']
 INTERNAL_IPS = ('0.0.0.0','127.0.0.1','localhost',)
 
 INSTALLED_APPS = [
     'app',
+    'leaflet',
+    'djgeojson',
     'material',
     'material.admin',
     'imagekit',
