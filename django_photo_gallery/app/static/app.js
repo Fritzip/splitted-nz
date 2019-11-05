06d2452a6e5d2436c00ac7d6044d707401213d0f
Maxime Sainlot <maxime.sainlot@univ-lyon1.fr>
2019-11-06 02:59:51 +1300

    css of navbar icons and js of sidebar (open/close)

--- django_photo_gallery/app/static/app.js
+++ django_photo_gallery/app/static/app.js
@@ -1,48 +1,62 @@
-﻿$(document).ready(function () {
+﻿$( document ).ready( function() {
+  $( ".sidenav" ).sidenav( {
+    onCloseStart : function() {
+      $( '.fa-times' ).addClass( 'fa-bars' ).removeClass( 'fa-times' );
+    },
 
-    $(".sidenav").sidenav();
+    onOpenStart : function() {
+      $( '.fa-bars' ).addClass( 'fa-times' ).removeClass( 'fa-bars' );
+    }
+  } );
 
-    $('.tooltipped').tooltip();
+  $( "#sidenav-toggle" ).click( function() {
+    var elem     = $( '.sidenav' );
+    var instance = M.Sidenav.getInstance( elem );
+    if ( instance.isOpen ) {
+      $( '.sidenav' ).sidenav( 'close' );
+    } else {
+      $( '.sidenav' ).sidenav( 'open' );
+    }
+  } );
 
-});
+  $( '.tooltipped' ).tooltip();
+} );
 
 // Initializes PhotoSwipe.
-var pswpInit = function (startsAtIndex) {
+var pswpInit = function( startsAtIndex ) {
+  if ( !startsAtIndex ) { startsAtIndex = 0; }
 
-    if (!startsAtIndex) {
-        startsAtIndex = 0;
-    }
+  var pswpElement = document.querySelectorAll( '.pswp' )[0];
 
-    var pswpElement = document.querySelectorAll('.pswp')[0];
+  // commented the array bellow since the images array will be loaded from the
+  // server in variable called djangoAlbumImages.
 
-    // commented the array bellow since the images array will be loaded from the server
-    // in variable called djangoAlbumImages.
+  // build items array
+  // var items = [
+  //    {
+  //        src: 'https://placekitten.com/600/400',
+  //        w: 600,
+  //        h: 400
+  //    },
+  //    {
+  //        src: 'https://placekitten.com/1200/900',
+  //        w: 1200,
+  //        h: 900
+  //    }
+  //];
 
-    // build items array
-    //var items = [
-    //    {
-    //        src: 'https://placekitten.com/600/400',
-    //        w: 600,
-    //        h: 400
-    //    },
-    //    {
-    //        src: 'https://placekitten.com/1200/900',
-    //        w: 1200,
-    //        h: 900
-    //    }
-    //];
+  // find is images are loaded from the server.
+  if ( window.djangoAlbumImages && window.djangoAlbumImages.length > 0 ) {
+    // define options (if needed)
+    var options = {
+      // optionName: 'option value'
+      // for example:
+      index : startsAtIndex // start at first slide
+    };
 
-    // find is images are loaded from the server.
-    if (window.djangoAlbumImages && window.djangoAlbumImages.length > 0) {
-        // define options (if needed)
-        var options = {
-            // optionName: 'option value'
-            // for example:
-            index: startsAtIndex // start at first slide
-        };
-
-        // Initializes and opens PhotoSwipe
-        var gallery = new PhotoSwipe(pswpElement, PhotoSwipeUI_Default, window.djangoAlbumImages, options);
-        gallery.init();
-    }
+    // Initializes and opens PhotoSwipe
+    var gallery = new PhotoSwipe(
+      pswpElement, PhotoSwipeUI_Default, window.djangoAlbumImages, options );
+    gallery.init();
+  }
 }
