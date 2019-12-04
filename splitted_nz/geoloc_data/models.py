72d287a96b7c92f0923a43bde7a09ad3cfb4e98a
Maxime Sainlot <maxime.sainlot@univ-lyon1.fr>
2019-12-04 19:42:18 +1300

    Popup style in map

--- splitted_nz/geoloc_data/models.py
+++ splitted_nz/geoloc_data/models.py
@@ -6,13 +6,34 @@ from django.contrib.gis.db import models
 from photo_gallery.models import Article
 from datetime import datetime, timedelta
 
-def event_date(start, end):
+def event_date(start, end=None):
     if end is None or start == end:
         return start.strftime("%d %b %Y")
     elif start.strftime("%b") == end.strftime("%b"):
         return start.strftime("%d") + " - " + end.strftime("%d %b %Y") 
     else:
         return start.strftime("%d %b") + " - " + end.strftime("%d %b %Y")
+
+def popupheader(title, icon, date, time=None):
+    htmltime = ''
+    if time:
+        htmltime = '<i class="fas fa-clock"></i>{}'.format(time)
+    return '''<div class="grid-popup-row">
+                        <b style="font-size:14px;">{1}</b>
+                   </div>
+                   <div class="grid-popup-row" style="justify-content: flex-start;">
+                        <i class="fas fa-calendar-alt"></i>
+                        {2}{3}
+                   </div>'''.format(icon, title, date, htmltime)
+                        # <i class="fas {0}" style="font-size:16px;"></i>
+                    # <i class="fas {0}" style="visibility:hidden;"></i>
+
+def popuparticlebtn(letters, slug, title):
+    return '''<a class="article-link-popup flex-popup-row nowrap btn waves-effect waves-light" data-letters="{}" href="{}">
+                            <i class="fas fa-newspaper"></i>
+                            <b>{}</b>
+                            <i class="fas fa-angle-right"></i>
+                        </a>'''.format(letters, reverse('photo_gallery:article', args=(slug,)), title)
 
 class DatedSpot(models.Model):
     article = models.ForeignKey(Article, on_delete=models.SET_NULL, blank=True, null=True)
@@ -25,24 +46,12 @@ class DatedSpot(models.Model):
 
     @property
     def popupContent(self):
-        popup = '''<div class="grid-popup-row">
-                        <i class="fas {}"></i>
-                        <b>{}</b>
-                        <i class="fas {}" style="visibility:hidden;"></i>
-                   </div>
-                   <div class="grid-popup-row" style="justify-content: flex-start;">
-                        <i class="fas {}"></i>
-                        {}
-                   </div>'''.format("fa-bed", self.name, "fa-bed", "fa-calendar-alt", event_date(self.start_date, self.end_date))
+        popup = popupheader(self.name, "fa-bed", event_date(self.start_date, self.end_date))
         
         if self.article:
-            popup += '''<a class="article-link-popup flex-popup-row btn waves-effect waves-light" data-letters="{}" href="{}">
-                            <i class="fas fa-newspaper"></i>
-                            <b>{}</b>
-                            <i class="fas fa-angle-right"></i>
-                        </a>'''.format(self.article.letters, reverse('photo_gallery:article', args=(self.article.slug,)), self.article.title)
+            popup += popuparticlebtn(self.article.letters, self.article.slug, self.article.title)
 
-        popup += '<a class=" zoom-in-popup btn-floating btn-small waves-effect waves-light"><i class="fas fa-compress-arrows-alt"></i></a>'
+        popup += '<a class="zoom-in-popup btn-floating btn-small waves-effect waves-light"><i class="fas fa-compress-arrows-alt"></i></a>'
 
         return popup
 
@@ -67,11 +76,11 @@ class GPXTrack(models.Model):
 
     BIKE = 'biking'
     RUN = 'running'
-    HIKE = 'hicking'
+    HIKE = 'hiking'
     STATUS = (
-       (BIKE, 'Mountain biking'),
-       (RUN, 'Trail run'),
-       (HIKE, 'Hicking'),
+       (BIKE, 'Bike'),
+       (RUN, 'Run'),
+       (HIKE, 'Hike'),
     )
     status = models.CharField(
        max_length=32,
@@ -82,9 +91,9 @@ class GPXTrack(models.Model):
     )
     elevation_gain = models.FloatField(blank=True, null=True)
     distance = models.FloatField(blank=True, null=True)
-    start_time = models.TimeField(blank=True, null=True)
-    end_time = models.TimeField(blank=True, null=True)
-    moving_time = models.TimeField(blank=True, null=True)
+    start_date = models.DateTimeField(blank=True, null=True)
+    end_date = models.DateTimeField(blank=True, null=True)
+    moving_time = models.DurationField(blank=True, null=True)
     avg_speed = models.FloatField(blank=True, null=True)
     article = models.ForeignKey(Article, on_delete=models.SET_NULL, blank=True, null=True)
 
@@ -101,47 +110,51 @@ class GPXTrack(models.Model):
         else:
             return "-"
 
+    def get_activity_icon(self):
+        if self.status == self.BIKE:
+            return "fa-biking"
+        elif self.status == self.RUN:
+            return "fa-running"
+        elif self.status == self.HIKE:
+            return "fa-hiking"
+        else:
+            return "fa-question"
+
+    def get_formated_date(self, datetime):
+        return datetime.strftime("%d/%m") if datetime else ""
+
+    def get_formated_time(self, datetime):
+        return datetime.strftime("%H:%M") if datetime else ""
+
     @property
     def popupContent(self):
-        popup = '''<div>
-                    <div class="flex-popup-row">
-                        <i class="fas {}"></i><b>{}</b><i class="fas {}"></i>
-                    </div>
-                    <div class="flex-popup-row">
-                        <div class="flex-popup-col">
+        popup = popupheader(self.name, self.get_activity_icon(), event_date(self.start_date), self.get_formated_time(self.start_date))
+
+        popup += '''<div class="flex-popup-row">
+                        <div class="card-panel flex-popup-col">
                           <i class="fas {}"></i>{}
                         </div>
-                        <div class="flex-popup-col">
+                        <div class="card-panel flex-popup-col">
                           <i class="fas {}"></i>{}
                         </div>
                     </div>
                     <div class="flex-popup-row">
-                        <div class="flex-popup-col">
+                        <div class="card-panel flex-popup-col">
                           <i class="fas {}"></i>{}
                         </div>
-                        <div class="flex-popup-col">
-                          <i class="fas {}"></i>{}
-                        </div>
-                        <div class="flex-popup-col">
+                        <div class="card-panel flex-popup-col">
                           <i class="fas {}"></i>{}
                         </div>
                     </div>
-                '''.format( "fa-biking", self.name, "fa-route",
-                            "fa-arrow-up", self.get_formated_stat(self.elevation_gain, "m"),
+                '''.format( "fa-arrow-up", self.get_formated_stat(self.elevation_gain, "m"),
                             "fa-ruler-horizontal", self.get_formated_stat(self.distance, "km"),
-                            "fa-hourglass-start", self.get_formated_stat(self.start_time, ""),
                             "fa-stopwatch", self.get_formated_stat(self.moving_time, ""),
-                            "fa-hourglass-end", self.get_formated_stat(self.end_time, ""),
                             "fa-tachometer-alt", self.get_formated_stat(self.avg_speed, "km/h"))
-                
         
         if self.article:
-            popup += '''<a class="article-link-popup flex-popup-row btn waves-effect waves-light" data-letters="{}" href="{}">
-                                <i class="fas fa-newspaper"></i><b>{}</b><i class="fas fa-angle-right"></i>
-                            </a>'''.format(self.article.letters, reverse('photo_gallery:article', args=(self.article.slug,)), self.article.title)
-
-        popup += '</div>'
-        # popup += '<span class=" zoom-in-popup btn-floating btn-small waves-effect waves-light"><i class="fas fa-compress-arrows-alt"></i></span>'
+            popup += popuparticlebtn(self.article.letters,self.article.slug,self.article.title) 
+            
+        popup += '<span class="top-left btn-floating btn-small red" style="cursor: default;"><i class="fas {}"></i></span>'.format(self.get_activity_icon())
 
         return popup
 
